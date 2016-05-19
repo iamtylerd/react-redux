4b053a1bb9ee0b17214d015d492c6732dcb68fad
iamtylerd <tylerdanielphotography@gmail.com>
2016-05-18 20:33:49 -0500

    finished app

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Photo from './Photo';
-// import comments
-
+import Comments from './Comments';
 
 
 
 const Single = React.createClass({
 	render() {
 		const { postId } = this.props.params;
-		const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
-		console.log(i);
+		const i = this.props.posts.findIndex((post) => post.code === postId);
+		const post = this.props.posts[i];
+
+		const postComments = this.props.comments[postId] || [];
 		return (
 			<div className="single-photo">
-			I am the single
+				<Photo i={i} post={post} {...this.props} />
+				<Comments postComments={postComments} {...this.props} />
 			</div>
 		)
 	}
